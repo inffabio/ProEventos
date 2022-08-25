d4b7ab20c02f832c02eecfb98b5b90e3fe1232cd
inffabio <inffabio#yahoo.com.br>
2022-08-24 21:33:43 -0300

    SEção 5: Angular, interpolação, Diretivas e Bindings

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AxiosInterceptorManager } from 'axios';
 
 @Component({
   selector: 'app-eventos',
@@ -8,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 80;
+  marginImg: number = 2;
+  isCollapsed: boolean = false;
+
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf( filtrarPor) !== -1 || evento.local.toLocaleLowerCase().indexOf( filtrarPor) !== -1
+    )
+  }
+
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -18,7 +43,11 @@ export class EventosComponent implements OnInit {
   public getEventos(): void {
 
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+
+      },
       error => console.log(error)
     )
 
